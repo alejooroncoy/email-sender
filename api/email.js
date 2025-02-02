5d76f04f2451413d8c9e7a38d503800336e45539
alejooroncoy <adoa2705@gmail.com>
2025-02-01 20:48:06 -0500

    fix: Update main

---
+++ api/email.js
@@ -0,0 +1,24 @@
+import { Resend } from "resend";
+import express from "express";
+
+const app = express();
+
+const resend = new Resend(process.env.API_KEY);
+
+app.post("/send", async (req, res) => {
+  const { to, subject, html } = req.body;
+  const { data, error } = await resend.emails.send({
+    from,
+    to,
+    subject,
+    html,
+  });
+
+  if (error) {
+    return res.status(400).json({ error });
+  }
+
+  res.status(200).json({ data });
+});
+
+export default app;
